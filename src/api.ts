c755fc8bac33be070cc4232325b38f728528b209
hejia <hejia@ludashi.com>
2020-06-03 09:25:37 +0800

    update-api

---
+++ src/api.ts
@@ -0,0 +1,26 @@
+const RequestQ = () => {}
+
+export function UserLogin (userName: string) {
+  interface Result {
+    errno: number,  //0表示登陆成功，其他表示失败
+    msg: string //登陆结果说明
+  }
+  return new Promise<Result>((resolve, reject) => {
+    RequestQ();
+  });
+}
+
+const Axios = async () => {}
+
+export async function agreement () {
+  let res:any
+  try {
+    res = await Axios();
+  } catch (e) {
+    console.log(e)
+  }
+  if (res && res.data) {
+    return res.data as string;
+  }
+  return '' as string;
+}
